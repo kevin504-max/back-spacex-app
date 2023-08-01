91f9e1e272084f8e08e7763f87e69bceac60b19d
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-01 16:44:48 -0300

    feat: lauches routes

--- router/launchesRoutes.js
+++ router/launchesRoutes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const LaunchController = require('../controllers/LaunchController');
+const launchController = new LaunchController();
+
+router.post('/launches', (request, response) => launchController.registerLaunches(request, response));
+router.get('/launches', (request, response) => launchController.getLaunches(request, response));
+router.get('/launches/:id', (request, response) => launchController.findLaunch(request, response));
 
 module.exports = router;
