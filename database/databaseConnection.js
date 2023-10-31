fcf3c41e56f9b73e01a2bbb12e1ca58eba4e0215
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-31 09:46:09 -0300

    feat: make database online

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const mongoose = require('mongoose')
 
 async function connect() {
     try {
         mongoose.set('strictQuery', true);
 
-        await mongoose.connect('mongodb://127.0.0.1:27017/spacex', {
+        await mongoose.connect(`
+            mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aai6u7s.mongodb.net/
+        `, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
